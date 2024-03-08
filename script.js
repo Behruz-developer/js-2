4b9411008517372726a78e3fc0fe1194f35dd6d0
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-03-08 20:47:11 +0500

    js-2

---
+++ script.js
@@ -0,0 +1,43 @@
+
+var name1 = prompt('Ismingizni kriting:')
+console.log("Mening ismim " + name1);
+
+var age = prompt('Yoshingizni kriting:')
+console.log("Men " + age + " yoshdaman");
+
+var misol1 = +prompt('Misolni yeching: 10 + 15 = ?')
+if(misol1 == 25){
+    console.log('Sizning jovob to\'g\'ri ' + misol1);
+}else{
+    console.log('Sizning jovob noto\'g\'ri ' + misol1 + " To'g'ri javob 25");
+}
+
+var misol2 = prompt('Misolni yeching: 55 - 14 = ?')
+if(misol2 == 41){
+    console.log('Sizning jovob to\'g\'ri ' + misol2);
+}else{
+    console.log('Sizning jovob noto\'g\'ri ' + misol2 + " To'g'ri javob 41");
+}
+
+var misol3 = prompt('Misolni yeching: 7 * 6 = ?')
+if(misol3 == 42){
+    console.log('Sizning jovob to\'g\'ri ' + misol3);
+}else{
+    console.log('Sizning jovob noto\'g\'ri ' + misol3 + " To'g'ri javob 42");
+}
+
+var misol4 = prompt('Misolni yeching: 48 / 6 = ?')
+if(misol4 == 8){
+    console.log('Sizning jovob to\'g\'ri ' + misol4);
+}else{
+    console.log('Sizning jovob noto\'g\'ri ' + misol4 + " To'g'ri javob 8");
+}
+
+var misol5 = prompt('Misolni yeching: 55 % 2 = ?')
+if(misol4 == 1){
+    console.log('Sizning jovob to\'g\'ri ' + misol4);
+}else{
+    console.log('Sizning jovob noto\'g\'ri ' + misol4 + " To'g'ri javob 1");
+}
+
+alert('Natijani kor\'ish uchun konsolni oching')
